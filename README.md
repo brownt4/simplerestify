2ee79b538ce8c35cdf732d0c1891c717aec004e8
Thomas Brown <thomas.k.brown12@gmail.com>
2016-05-20 18:18:46 -0600

    Working server-side and client-side authenticated form.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Sample Restify App
+# Sample Restify server
 
-This is a basic restify app. It serves up a static html file.
+This is a basic restify server. It serves up a static html file.
 
-This app requires node. Install dependencies with `npm install`.
+This server requires node. Install dependencies with `npm install`.
 
 To run the server, use `node server.js`. Files will be served from `localhost:8000`.
 
-# Validation app
+# Validation server
 
-For the validation app, we need a user registration with the following fields:
+For the validation server, we need a user registration with the following fields:
 * User Name
   * Must have a minimum length of 6 characters
 * First Name
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-var restify = require('restify');
+var util = require('util'),
+    restify = require('restify'),
+    restifyValidator = require('restify-validator');
 
-var server = restify.createServer();
-server.get(/.*/, restify.serveStatic({
-    'directory': 'src/html/',
-    'default': 'index.html'
- }));
+var server = restify.createServer({
+     name: 'simplerestify'
+});
+
+
+server.use(restify.bodyParser());
+server.use(restify.queryParser());
+server.use(restifyValidator);
+
+server.post('/', function(req, res) {
+  req.assert('firstname', 'Invalid first name').isAlpha();
+  req.assert('lastname', 'Invalid last name').isAlpha();
+  req.assert('email', 'Invalid email address').isEmail();
+  req.assert('bday', 'Invalid birth day').isDate();
+  req.assert('username', 'Invalid user name').is(/.{6,}/);
+  req.assert('pass1', 'Invalid password').is(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).+$/).equals(req.params.pass2);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    res.send(500 ,'There have been validation errors: ' + util.inspect(errors));
+    return;
+  }
+  res.send(201);
+});
 
 server.listen(8000, function() {
   console.log('%s listening at %s', server.name, server.url);
