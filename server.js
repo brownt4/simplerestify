7f46cb5b7ed6e669f7da7c812f2ad6a763069e5f
Thomas Brown <thomas.k.brown12@gmail.com>
2016-05-20 19:54:27 -0600

    Fix email validation on client
    HTML’s `type=email` does not catch if an email doesn’t contain a `.`.

--- server.js
+++ server.js
@@ -3,34 +3,34 @@ var util = require('util'),
     restifyValidator = require('restify-validator');
 
 var server = restify.createServer({
-     name: 'simplerestify'
+    name: 'simplerestify'
 });
 
 server.get(/.*/, restify.serveStatic({
     'directory': 'src/html/',
     'default': 'index.html'
- }));
+}));
 
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
 server.use(restifyValidator);
 
 server.post('/', function(req, res) {
-  req.assert('firstname', 'Invalid first name').isAlpha();
-  req.assert('lastname', 'Invalid last name').isAlpha();
-  req.assert('email', 'Invalid email address').isEmail();
-  req.assert('bday', 'Invalid birth day').isDate();
-  req.assert('username', 'Invalid user name').is(/.{6,}/);
-  req.assert('pass1', 'Invalid password').is(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).+$/).equals(req.params.pass2);
+    req.assert('firstname', 'Invalid first name').isAlpha();
+    req.assert('lastname', 'Invalid last name').isAlpha();
+    req.assert('email', 'Invalid email address').isEmail();
+    req.assert('bday', 'Invalid birth day').isDate();
+    req.assert('username', 'Invalid user name').is(/.{6,}/);
+    req.assert('pass1', 'Invalid password').is(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).+$/).equals(req.params.pass2);
 
-  var errors = req.validationErrors();
-  if (errors) {
-    res.send(500 ,'There have been validation errors: ' + util.inspect(errors));
-    return;
-  }
-  res.send(201);
+    var errors = req.validationErrors();
+    if (errors) {
+        res.send(500, 'There have been validation errors: ' + util.inspect(errors));
+        return;
+    }
+    res.send(201);
 });
 
 server.listen(8000, function() {
-  console.log('%s listening at %s', server.name, server.url);
+    console.log('%s listening at %s', server.name, server.url);
 });
